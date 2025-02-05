ed340463d2a4323b25e2e4dc689cd0ecd37de86b
SOUL KILLER OFC <153274881+Ayeshherath@users.noreply.github.com>
2025-02-05 20:47:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
-ANTI_DELETE: process.env.ANTI_DELETE || "true",
-ANTI_CALL: process.env.ANTI_CALL || "true",
-FOOTER: process.env.FOOTER || "*◆─〈 ✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦ 〉─◆*",
+STATUS_READ_MSG: process.env.STATUS_READ_MSG || "podda  md",
+FOOTER: process.env.FOOTER || "*podda md*",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/XDeIUwP.jpeg",
+SESSION_ID: process.env.SESSION_ID || "6WhFzToS#olvxdmaAU625-J_m4Emhiv8A9JW8bDlE7--gnDB4OLY",
 };
 
 
